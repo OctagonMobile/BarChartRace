d55ebde7596a58527146befc6148d02bdc16f433
Kishore Kumar <kishore.kumar@pax-ai.ae>
2020-06-21 19:29:23 +0400

    Thousand Separator Added, Decimal Places to 2 and If integer donot show 0s

--- BarChartRace/Classes/DataEntry.swift
+++ BarChartRace/Classes/DataEntry.swift
@@ -30,9 +30,36 @@ public struct DataEntry {
     public init(color: UIColor, height: Float, textValue: String, textValueFont: UIFont = UIFont.systemFont(ofSize: 14), title: String, titleValueFont:UIFont = UIFont.systemFont(ofSize: 14)) {
         self.color = color
         self.height = height
-        self.textValue = textValue
         self.textValueFont = textValueFont
         self.title = title
         self.titleValueFont = titleValueFont
+       
+        var doubleVal = Double(textValue)
+        doubleVal = doubleVal?.round(to: 2)
+        if let val = doubleVal, val.isInteger {
+            self.textValue = Formatter.withSeparator.string(from: Int(val) as NSNumber) ?? ""
+        } else {
+            self.textValue = Formatter.withSeparator.string(from: (doubleVal ?? 0) as NSNumber) ?? ""
+        }
     }
+}
+
+extension Double {
+    var isInteger: Bool {
+        return floor(self) == self
+    }
+    
+    func round(to places: Int) -> Double {
+           let divisor = pow(10.0, Double(places))
+           return (self * divisor).rounded() / divisor
+    }
+}
+
+extension Formatter {
+    static let withSeparator: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.groupingSeparator = ","
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
 }
