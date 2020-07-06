338fae80ba84fcc24999d9cffc65242a360f82a5
rameez <rmz.rmz@live.com>
2020-07-06 20:21:55 +0400

    Added json file for data

--- Example/BarChartRace/ViewController.swift
+++ Example/BarChartRace/ViewController.swift
@@ -11,28 +11,21 @@ import BarChartRace
 
 class ViewController: UIViewController {
     
-//    @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var barChart: BasicBarChart!
     
-    private let numSet = 10
-    private let numEntry = 10
-
+    var dataSets: [DataSet] =   []
+    
     //MARK: Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        readFromJson()
+        
         barChart.delegate = self
-        let dataSets = generateRandomDataSets()
         barChart.setupBarChartRace(dataSets, animated: true)
         barChart.titleColor = .black
         barChart.valueColor = .black
-//        barChart.timeInterval = 1.0
-//
-//        slider.isContinuous = true
-//        slider.value = 0
-//        slider.minimumValue = 0.0
-//        slider.maximumValue = Float(dataSets.count - 1)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -41,36 +34,41 @@ class ViewController: UIViewController {
     }
         
     //MARK: Private Functions
-    /// Generates Random Data Sets.
-    /// - Returns: List of Datset.
-    private func generateRandomDataSets() -> [DataSet] {
-        var result: [DataSet] = []
-        for i in 0..<numSet {
+    
+    private func readFromJson() {
+        guard let path = Bundle.main.path(forResource: "data", ofType: "json")  else { return }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+            if let jsonResult = jsonResult as? [[String: Any]] {
 
-            var date = Date()
-            date.addTimeInterval(TimeInterval(24*60*60*i))
+                for data in jsonResult {
+                    let dateString = data["date"] as? String
+                    let entries = data["entries"] as? [[String: Any]] ?? []
 
-            let entriesList: [DataEntry] = generateRandomDataEntries()
-            let set = DataSet(date, dataEntries: entriesList)
-            result.append(set)
+                    guard let date = dateString?.formattedDate("yyyy-MM-dd") else { continue }
+                    
+                    let colors = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
+                    var entriesList: [DataEntry] = []
+                    
+                    let maxEntryValue = entries.sorted(by: { $0["value"] as! Float > $1["value"] as! Float}).first?["value"] as? Float ?? 0.0
+                    for (index, entry) in entries.enumerated() {
+                        let title = entry["title"] as? String ?? ""
+                        let value = entry["value"] as? Float ?? 0.0
+                        
+                        let height: Float = maxEntryValue > 0 ? Float(value) / maxEntryValue : 0
+
+                        let dataEntry = DataEntry(color: colors[index % colors.count], height: height, textValue: "\(value)", title: title)
+                        entriesList.append(dataEntry)
+                    }
+                    dataSets.append(DataSet(date, dataEntries: entriesList))
+                }
+                
+            }
+        } catch {
+            // handle error
+            print("\(error.localizedDescription)")
         }
-
-        return result
-    }
-
-    /// Generates Random Data Entry.
-    /// - Returns: List of DataEntry.
-    private func generateRandomDataEntries() -> [DataEntry] {
-        let colors = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
-        var result: [DataEntry] = []
-        for i in 0..<numEntry {
-            let value = (arc4random() % 90) + 10
-            let height: Float = Float(value) / 100.0
-            
-            result.append(DataEntry(color: colors[i % colors.count], height: height, textValue: "\(value)", title: "Title \(i)"))
-        }
-            
-        return result
     }
     
     private func updateDateOnUI(_ date: Date) {
@@ -100,18 +98,17 @@ extension ViewController: BarChartRaceDelegate {
     }
 
     func currentDataSet(_ dataSet: DataSet, index: Int) {
-
         updateDateOnUI(dataSet.date)
-
-//        if barChart.playerState == .playing {
-////            let calculatedValue = Float(index) / Float(numSet)
-////            self.slider.setValue(calculatedValue, animated: true)
-//
-//            let animationTime = barChart.timeInterval //+ 0.5 // 0.5 is added to make it smoother
-//            UIView.animate(withDuration: animationTime) {
-//                let calculatedValue = Float(index) / Float(self.numSet-1)
-//                self.slider.setValue(Float(calculatedValue), animated: true)
-//            }
-//        }
     }
+}
+
+
+extension String {
+    
+    func formattedDate(_ format: String) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.date(from: self)
+    }
+    
 }
